0af279ae406b7a0b00aa7c3d45cc0bbe0f22539b
Jovi <nean@protonmail.com>
2023-08-23 23:06:26 -0300

    feat: home screen base

---
+++ lib/features/inicio/inicio.dart
@@ -0,0 +1,103 @@
+import "package:flutter/material.dart";
+import "package:pocpay/features/pagamento/pagamento.dart";
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  double saldo = 120.00;
+  String exibicaoSaldo = "******";
+  bool mostrandoSaldo = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("PocPay"),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.only(top: 20, left: 20),
+        children: [
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Olá fulano!",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Saldo: R\$ $exibicaoSaldo",
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 60, top: 20),
+                child: OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      mostrandoSaldo
+                          ? exibicaoSaldo = saldo.toString()
+                          : exibicaoSaldo = "******";
+                      mostrandoSaldo = !mostrandoSaldo;
+                    });
+                  },
+                  child: Text(mostrandoSaldo ? "Exibir Saldo" : "Ocultar Saldo",
+                      style: const TextStyle(color: Colors.green)),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.attach_money),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const PagamentoScreen(),
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedIconTheme: IconThemeData(color: Colors.green),
+        currentIndex: 1,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Inicio",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            backgroundColor: Colors.green,
+            label: "Pagar",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet),
+            label: "Carteira",
+          ),
+        ],
+      ),
+    );
+  }
+}
