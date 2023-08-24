adc81a12de18cfa8d4a59af30807a56559db0cdd
Jovi <nean@protonmail.com>
2023-08-23 23:06:45 -0300

    feat: paying screen

---
+++ lib/features/pagamento/pagamento.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class PagamentoScreen extends StatelessWidget {
+  const PagamentoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("Pagar"),
+        ),
+        body: const Center());
+  }
+}
